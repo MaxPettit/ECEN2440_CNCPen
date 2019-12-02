89291a30b834e7a8a53e69dfa9e8b0f7575b704e
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-02 11:47:50 -0700

    Add files via upload

---
+++ read_js.h
@@ -0,0 +1,15 @@
+/*
+ * read_js.h
+ *
+ *  Created on: Dec 1, 2019
+ *      Author: dafre
+ */
+
+#ifndef READ_JS_H_
+#define READ_JS_H_
+
+void config_js();
+
+void js_position();
+
+#endif /* READ_JS_H_ */
