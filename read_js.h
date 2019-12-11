b7b82e8dadadf7ec081d0b83e9d1b7b1fa4322e3
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-11 12:03:40 -0700

    Update read_js.h

--- read_js.h
+++ read_js.h
@@ -2,7 +2,7 @@
  * js_test.h
  *
  *  Created on: Dec 4, 2019
- *      Author: dafre
+ *      Author: Nathan
  */
 
 #ifndef READ_JS_H_
@@ -14,7 +14,11 @@
 #include <stdint.h>
 
 extern volatile uint16_t _nadc[32];
+
+// Config xout and yout pins on the joystick to MSP pins for ADC reading
 void joystick_configure(void);
+
+// Experimentally get ADC values to determine semi-accurate values that correspond to joystick positions
 int8_t joysticklocation(uint8_t channelx, uint8_t channely);
 
 #endif /* READ_JS_H_ */
