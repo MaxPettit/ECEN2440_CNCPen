a82f1c22bafaa25a5cef34a269196d64b8f24940
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-11 11:49:18 -0700

    Update led_test.h

--- led_test.h
+++ led_test.h
@@ -2,12 +2,14 @@
  * led_test.h
  *
  *  Created on: Dec 6, 2019
- *      Author: dafre
+ *      Author: Nathan
  */
 
 #ifndef LED_TEST_H_
 #define LED_TEST_H_
 
+
+// Changes color of LED based on joystick position for testing and debugging
 led_color(volatile uint8_t color);
 
 
