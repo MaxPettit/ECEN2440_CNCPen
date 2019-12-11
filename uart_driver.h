95d8bd90934c57a19442383e0ffe68c6871f73b9
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-12-11 12:34:21 -0700

    Update uart_driver.h

--- uart_driver.h
+++ uart_driver.h
@@ -8,9 +8,16 @@
 #ifndef UART_DRIVER_H_
 #define UART_DRIVER_H_
 
+/* Configure eUSCI_A2 */
 void uart_config();
+
+/* Write to LCD, returns 1 if error, 0 otherwise
+ *    Error will only occur if trying to print empty string
+ *    For most uses, output is ignored
+ */
 int uart_write(const char *str);
-void uart_char(const int c);
+
+/* Send characters that cause LCD to clear display */
 void lcd_clear();
 
 #endif /* UART_DRIVER_H_ */
