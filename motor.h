06259302de0e887537fe75b5f7c86b26a3216f45
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-11 12:24:39 -0700

    Update motor.h

--- motor.h
+++ motor.h
@@ -2,7 +2,7 @@
  * motor.h
  *
  *  Created on: Dec 7, 2019
- *      Author: maxpettit
+ *      Author: maxpettit, Nathan
  */
 
 #ifndef MOTOR_H_
@@ -11,7 +11,13 @@
 #define x_pwm 0
 #define y_pwm 1
 
+/*
+   Starts and stops motor pwm based on joystick position
+   Determines direction of motors based on joystick position
+*/
 void motor_pos(volatile uint8_t pos);
+
+// Toggles direction of motors based on joystick position
 void set_dir(char c, int t);
 
 #endif /* MOTOR_H_ */
