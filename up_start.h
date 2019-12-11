5f2e8791aac93aec0cd35bce011088997178bd73
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-12-11 12:39:54 -0700

    Update up_start.h

--- up_start.h
+++ up_start.h
@@ -8,10 +8,14 @@
 #ifndef UP_START_H_
 #define UP_START_H_
 
+/* Calls functions to configure many components
+ *     UART, Timers, Interrupts, etc  */
 void startup_config(void);
 
+/* Sets pen to up and displays startup message */
 void startup_go(void);
 
+/* Writes startup message to LCD*/
 void start_msg(void);
 
 #endif /* UP_START_H_ */
