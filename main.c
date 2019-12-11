b7afa6c5897de755609c882b2386f775171fbb7a
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-11 12:40:40 -0700

    Update main.c

--- main.c
+++ main.c
@@ -19,49 +19,31 @@ volatile uint8_t value;
 
 
 void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+    WDTCTL = WDTPW | WDTHOLD;                                       // Stop WDT
     SCB->SCR &= ~SCB_SCR_ENABLE_SLEEPONEXIT;
 
-    startup_config();
-    config_drv_gpio();
-    startup_go();
+    startup_config();                                               // Config PWM, servo, and LCD
+    config_drv_gpio();                                              // Config motor driver board GPIO
+    startup_go();                                                   // Starts servo PWM and displays start-up message
 
 #ifdef JOYSTICK
-    joystick_configure(); // Configure Joystick P1 stuff
+    joystick_configure();                                           // Configure Joystick
 #endif
-/*#ifdef LED
-    config_LED();
-#endif*/
+
 #ifdef ADC
-    ADC_init(); // Analog to digital configuration
-    ADC_addChannel(0,15,0); // MEM0 x dir
-    ADC_addChannel(4,9,0); // MEM4 y dir
-    ADC_EOS(15); // Enable EOS
-    ADC_start(); // Enable ADC
+    ADC_init();                                                     // Config ADC
+    ADC_addChannel(0,15,0);                                         // Set MEM0 for x-axis
+    ADC_addChannel(4,9,0);                                          // Set MEM4 for y-yaxis
+    ADC_EOS(15);                                                    // Enable EOS
+    ADC_start();                                                    // Enable ADC
 #endif
-    __enable_irq(); // Enable Interrupts
+    __enable_irq();                                                 // Enable Interrupts
 
-   // start_pwm(x_pwm);
-  //      P2->OUT |= BIT6;
-
-    while(1){
+    while(1){                                                       // While loop to keep program running and get ADC readings
 #ifdef JOYSTICK
-//        for(int i=0; i<1000; i++);
-        value = jslocation(0,4);
-        motor_pos(value);
-        led_color(value);
-
-
+        value = jslocation(0,4);                                    // Set a value based on joystick position
+        motor_pos(value);                                           // Moves motor based on joystick position
+        led_color(value);                                           // Changes LED based on joystick position
 #endif
     }
-
-//    int i;
-//    for(i=0; i<1000000; i++);
-//    config_drv_gpio();
-//    for(i=0; i<10000; i++);
-//    start_pwm(0);
-//    start_pwm(1);
-//
-//    while(1);
-
 }
