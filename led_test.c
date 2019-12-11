76cb2d51c3bed2ccc95e8a561ebdb920a4ad1149
nael3505 <55171733+nael3505@users.noreply.github.com>
2019-12-11 11:47:47 -0700

    Update led_test.c

--- led_test.c
+++ led_test.c
@@ -2,28 +2,28 @@
  * led_test.c
  *
  *  Created on: Dec 6, 2019
- *      Author: dafre
+ *      Author: Nathan
  */
 #include "msp.h"
 
 void led_color(volatile uint8_t color){
-    P2->DIR |= 0x07;
-    P2->OUT = 0;
+    P2->DIR |= 0x07;                            // Enable LED
+    P2->OUT = 0;                                // Set LED off
 
-    if(color == 4)
-        P2->OUT = 0x05;
-    if(color == 3)
-        P2->OUT = 0x06;
-    if(color == 2)
-        P2->OUT = 0x07;
-    if(color == 1)
-        P2->OUT = 0x04;
-    if(color == 6)
-        P2->OUT = 0x03;
-    if(color == 5)
-        P2->OUT = 0x02;
-    if(color == 7)
-        P2->OUT = 0x01;
-    if(color == 0)
-        P2->OUT = 0;
+    if(color == 4)                              // Joystick Right
+        P2->OUT = 0x05;                         // LED Color: Pink
+    if(color == 3)                              // Joystick Left
+        P2->OUT = 0x06;                         // LED Color: Aqua
+    if(color == 2)                              // Joystick Up
+        P2->OUT = 0x07;                         // LED Color: White
+    if(color == 1)                              // Joystick Down
+        P2->OUT = 0x04;                         // LED Color: Blue
+    if(color == 6)                              // Joystick Bottom Right
+        P2->OUT = 0x03;                         // LED Color: Yellow
+    if(color == 5)                              // Joystick Top Left
+        P2->OUT = 0x02;                         // LED Color: Green
+    if(color == 7)                              // Joystick Bottom Left
+        P2->OUT = 0x01;                         // LED Color: Red
+    if(color == 0)                              // Joystick in Nuetral Position
+        P2->OUT = 0;                            // Set LED off
 }
